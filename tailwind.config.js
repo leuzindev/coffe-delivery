14f7e198a8116e23b7f854645fd8c4dcc5edc4fa
Leonardo_Soares <leonardoc.soares08@gmail.com>
2023-06-02 07:52:14 -0300

    setup: fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,54 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        body: 'var(--font-roboto)',
+        alt: 'var(--font-baloo)',
+      },
+      colors: {
+        'yellow-dark': '#C47F17',
+        yellow: '#DBAC2C',
+        'yellow-light': '#F1E9C9',
+
+        'purple-dark': '#4B2995',
+        purple: '#8047F8',
+        'purple-light': '#EBE5F9',
+
+        'base-title': '#272221',
+        'base-subtitle': '#403937',
+        'base-text': '#574F4D',
+        'base-label': '#8D8686',
+        'base-hover': '#D7D5D5',
+
+        'base-button': '#E6E5E5',
+        'base-input': '#EDEDED',
+        'base-card': '#F3F2F2',
+        background: '#FAFAFA',
+        white: '#FFFFFF',
+      },
+      fontSize: {
+        'title-xl': '48px',
+        'title-l': '32px',
+        'title-m': '24px',
+        'title-s': '20px',
+        'title-xs': '18px',
+
+        'text-l': '20px',
+        'text-m': '16px',
+        'text-s': '14px',
+        'text-xs': '12px',
+
+        tag: '10px',
+        'button-g': '14px',
+        'button-m': '12px',
       },
     },
   },
   plugins: [],
 }
+
+// '2xl': ['1.5rem', {
+//   lineHeight: '2rem',
+//   letterSpacing: '-0.01em',
+//   fontWeight: '500',
+// }],
