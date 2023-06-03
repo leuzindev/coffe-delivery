48d78ff5d5ed202bcc2bf6f8bd000df4139b199f
leuzindev <leonardoc.soares08@gmail.com>
2023-06-03 15:11:50 -0300

    fix: container for responsive

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from 'react'
 import LogoCoffe from '../../assets/LogoCoffeDelivery.svg'
 import Image from 'next/image'
-import { CartButton } from '@/components/CartButton'
-import AddresButton from '@/components/AddresButton'
+import { CartButton } from '../../components/CartButton'
+import AddresButton from '../../components/AddresButton'
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <header className="fixed top-0 flex h-[104px] w-full items-center bg-background ">
-        <div className="m-auto flex w-[70%] items-center justify-between sm:w-[90%]">
+    <div className="flex flex-col items-center justify-center">
+      <header className="fixed top-0 flex h-[104px]  w-full max-w-[74rem] items-center bg-background">
+        <div className="m-auto flex  w-full items-center justify-between lg:w-[90%] ">
           <Image src={LogoCoffe} alt="Copo de café com grãos atras" />
           <div className="flex gap-3">
             <AddresButton address="Cajamar, SP" />
@@ -18,7 +18,9 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
       </header>
       <div className="h-[104px]"></div>
 
-      <div className="min-h-screen bg-background">{children}</div>
-    </>
+      <div className="m-auto min-h-screen w-full max-w-[74rem] bg-background">
+        {children}
+      </div>
+    </div>
   )
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className=""></div>
+  return (
+    <div className="flex flex-wrap">
+      <h1>Container Hme</h1>
+    </div>
+  )
 }
