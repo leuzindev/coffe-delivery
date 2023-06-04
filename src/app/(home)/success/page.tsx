d59a692a7a7dcc5ddb36642c05598d6e5f4c176a
leuzindev <leonardoc.soares08@gmail.com>
2023-06-04 16:56:55 -0300

    fix: ajuste branch conflicts

--- src/app/(home)/success/page.tsx
+++ src/app/(home)/success/page.tsx
@@ -1,46 +1,83 @@
-import { FaMapMarkerAlt } from 'react-icons/fa'
+import { FaMapMarkerAlt, FaStopwatch, FaDollarSign } from 'react-icons/fa'
+import { IconTextWidget } from '../../../components/IconTextWidget'
+import FinishSvg from '../../../assets/Finish.svg'
+import Image from 'next/image'
+
 export default function Success() {
+  const classesDivCard = [
+    'my-10',
+    'flex',
+    'h-[270px]',
+    'w-[526px]',
+    'flex-col',
+    'items-start',
+    'gap-8',
+    'rounded-bl-[36px]',
+    'rounded-br-lg',
+    'rounded-tl-lg',
+    'rounded-tr-[36px]',
+    'border',
+    'p-10',
+    'md:w-full',
+  ]
   return (
     <main>
-      <h1 className="mt-[80px] font-alt text-title-l text-yellow-dark ">
-        Uhu! Pedido confirmado
-      </h1>
-      <p className="mt-[4px] font-body text-text-l text-base-subtitle">
-        Agora é só aguardar que logo o café chegará até você
-      </p>
-      <div className="my-10 flex h-[270px] w-[526px] flex-col items-start gap-8 rounded-bl-[36px] rounded-br-lg rounded-tl-lg rounded-tr-[36px] border p-10">
-        <div className="flex">
-          <span className="flex h-[32px] w-[32px] items-center justify-center rounded-[1000px] bg-purple p-2">
-            <FaMapMarkerAlt className="bg-purple text-lg" />
-          </span>
-          <p className="px-3 font-body text-text-m">
-            Entrega em Rua <strong>João Daniel Martinelli, 102</strong>
-            <br />
-            Farrapos - Porto Alegre, RS
+      <div className="flex">
+        <section className="w-1/2">
+          <h1 className="mt-[80px] font-alt text-title-l text-yellow-dark ">
+            Uhu! Pedido confirmado
+          </h1>
+          <p className="mt-[4px] font-body text-base-subtitle">
+            Agora é só aguardar que logo o café chegará até você
           </p>
-        </div>
+          <div className={classesDivCard.join(' ')}>
+            <div className="flex">
+              <IconTextWidget
+                icon={<FaMapMarkerAlt className="bg-purple text-lg" />}
+                title={
+                  <>
+                    Entrega em <strong>Rua João Daniel Martinelli, 102</strong>{' '}
+                    Farrapos - Porto Alegre, RS
+                  </>
+                }
+                color="bg-purple "
+              />
+            </div>
 
-        <div className="flex">
-          <span className="flex h-[32px] w-[32px] items-center justify-center rounded-[1000px] bg-purple p-2">
-            <FaMapMarkerAlt className="bg-purple text-lg" />
-          </span>
-          <p className="px-3 font-body text-text-m">
-            Previsão de entrega
-            <br />
-            <strong>20 min - 30 min</strong>
-          </p>
-        </div>
+            <div className="flex">
+              <IconTextWidget
+                icon={<FaStopwatch className="bg-yellow text-lg" />}
+                title={
+                  <>
+                    Previsão de entrega <strong>20 min - 30 minm</strong>
+                  </>
+                }
+                color="bg-yellow "
+              />
+            </div>
 
-        <div className="flex">
-          <span className="flex h-[32px] w-[32px] items-center justify-center rounded-[1000px] bg-purple p-2">
-            <FaMapMarkerAlt className="bg-purple text-lg" />
-          </span>
-          <p className="px-3 font-body text-text-m">
-            Pagamento na entrega
-            <br />
-            <strong>Cartão de Crédito </strong>
-          </p>
-        </div>
+            <div className="flex">
+              <IconTextWidget
+                icon={<FaDollarSign className="bg-yellow-dark text-lg" />}
+                title={
+                  <>
+                    Pagamento na entrega <strong>Cartão de Crédito</strong>
+                  </>
+                }
+                color="bg-yellow-dark "
+              />
+            </div>
+          </div>
+        </section>
+        <section className="flex w-1/2 items-center justify-center">
+          <Image
+            src={FinishSvg}
+            width={492}
+            height={293}
+            alt="Imagem de um cara de entrega de moto"
+            className="mt-36"
+          />
+        </section>
       </div>
     </main>
   )
